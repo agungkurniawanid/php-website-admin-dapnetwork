584e8f666d649c85a403de369aa50872d10f979d
Agung Kurniawan <110348967+AgungKurniawanCyberSecurity@users.noreply.github.com>
2023-12-03 16:12:30 +0700

    update

--- api/resetpassword.php
+++ api/resetpassword.php
@@ -1,11 +1,4 @@
 <?php
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require '../phpmailer/src/Exception.php';
-require '../phpmailer/src/PHPMailer.php';
-require '../phpmailer/src/SMTP.php';
 require 'connectionapi.php';
 
 // Set header untuk memberitahu bahwa respons yang dihasilkan adalah JSON
@@ -13,53 +6,69 @@ header('Content-Type: application/json');
 
 $response = array(); // Array untuk menyimpan respons
 
-if (!empty($_POST['email'])) {
-    $email = $_POST['email'];
+if (!empty($_POST['telfon'])) {
+    $telfon = $_POST['telfon'];
+
     $con = $connection_database;
     if ($con) {
         try {
             $OOTP = rand(100000, 999999);
         } catch (Exception $e) {
         }
-        $SQL_TBPEGAWAI = "SELECT * FROM tb_pegawai WHERE email = '$email'";
+        $SQL_TBPEGAWAI = "SELECT * FROM tb_pegawai WHERE nomor_telepon = '$telfon'";
         $PROSES_TB_PEGAWAI = mysqli_query($con, $SQL_TBPEGAWAI);
         $FETCH_TB_PEGAWAI = mysqli_fetch_assoc($PROSES_TB_PEGAWAI);
-        if(mysqli_num_rows($PROSES_TB_PEGAWAI) == 0) {
+
+
+        if (mysqli_num_rows($PROSES_TB_PEGAWAI) == 0) {
             $response['status'] = 'error';
             $response['message'] = 'User not found';
             echo json_encode($response);
             exit();
         }
+
         $ID_PEGAWAI = $FETCH_TB_PEGAWAI['id_pegawai'];
+        $NAMA_PEGAWAI = $FETCH_TB_PEGAWAI['nama_awal'] . ' ' . $FETCH_TB_PEGAWAI['nama_akhir'];
 
         $SQL_TBRESET = "INSERT INTO tb_reset_password (id_pegawai, ootp) VALUES ('$ID_PEGAWAI', '$OOTP')";
         $PROSES_TB_RESET = mysqli_query($con, $SQL_TBRESET);
+
         if ($PROSES_TB_RESET) {
+
             if (mysqli_affected_rows($con) > 0) {
-                $mail = new PHPMailer();
-                try {
-                    // $mail = new PHPMailer(true);
-                    $mail->isSMTP();
-                    $mail->Host = 'smtp.gmail.com';
-                    $mail->SMTPAuth = true;
-                    $mail->Username = 'agungklewang26@gmail.com';
-                    $mail->Password = 'vftquhncmkhnghty';
-                    $mail->SMTPSecure = 'tls';
-                    $mail->Port = 587;
-                    $mail->SMTPDebug  = 2;
-                    $mail->setFrom('adgt1378@gmail.com');
-                    $mail->addAddress($email);
-                    $mail->isHTML(true);
-                    $mail->Subject = "RESET PASSWORD DAPNETWORK APP";
-                    $mail->Body = '<p style="color: #000; font-size: 24px font-weight:bold;"> KODE OTP ANDA : ' . $OOTP . '</p>';
-                    $mail->send();
-                    
+                // Send SMS via Fonnte API
+                $token = 'mzZ5v_rmG9TKTf1d-FIs';
+                $curl = curl_init();
+
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => 'https://api.fonnte.com/send',
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => '',
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => 'POST',
+                    CURLOPT_POSTFIELDS => array(
+                        'target' => $telfon,
+                        'message' => "RESET PASSWORD DAPNETWORK APP: \nHai $NAMA_PEGAWAI,\nKami ingin memberitahu Anda bahwa permintaan reset password Anda telah kami terima. Kode OTP Anda untuk mereset password adalah $OOTP. Silakan gunakan kode ini dalam aplikasi untuk mengatur ulang kata sandi Anda.\nJangan ragu untuk menghubungi tim dukungan kami jika Anda mengalami kesulitan atau memiliki pertanyaan lebih lanjut. Kami selalu siap membantu Anda.\nTerima kasih atas kepercayaan Anda pada DAPNETWORK APP!\nSalam hangat,\nTim Developer DAPNETWORK"
+                    ),
+                    CURLOPT_HTTPHEADER => array(
+                        'Authorization: ' . $token,
+                    ),
+                ));
+
+                $response_sms = curl_exec($curl);
+
+                curl_close($curl);
+
+                if ($response_sms) {
                     $response['status'] = 'success';
-                    $response['message'] = 'Email sent successfully';
+                    $response['message'] = 'SMS sent successfully';
                     echo json_encode($response);
-                } catch (Exception $e) {
+                } else {
                     $response['status'] = 'error';
-                    $response['message'] = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+                    $response['message'] = 'Failed to send SMS';
                     echo json_encode($response);
                 }
             }
@@ -67,7 +76,6 @@ if (!empty($_POST['email'])) {
     }
 } else {
     $response['status'] = 'error';
-    $response['message'] = 'Email parameter is missing';
+    $response['message'] = 'Phone number parameter is missing';
     echo json_encode($response);
 }
-?>
